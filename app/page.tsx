9dc206367169a26b3dded7e6be2fc6c6812a7319
Simrit <kaulsimrit@gmail.com>
2024-03-30 16:37:15 +0530

    Played around with colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-myBlue-100">
+        Hello World
+      </h1>
+      <h1 className="border-2 border-myBlue-200">
+        Hello World
+      </h1>
+      <h1 className="bg-indigo-950 text-amber-500 border-4 border-amber-400">
+        Hello World
+      </h1>
+      <h1 className="bg-myBlue-200 text-white border-2 border-myBlue-100">
         Hello World
       </h1>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "myBlue": {
+          100: "#546ba8",
+          200: "#2f4da1",
+        }
+      }
     },
   },
   plugins: [],
